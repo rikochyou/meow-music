8a0447d5e0d525161d0719703e41a831f60a903b
riko <rikochyou@outlook.com>
2019-12-05 17:58:05 +0800

    tcb-router路由优化

--- miniprogram/pages/playlist/playlist.js
+++ miniprogram/pages/playlist/playlist.js
@@ -1,4 +1,5 @@
 // pages/playlist/playlist.js
+const MAX_LIMIT = 15
 Page({
 
   /**
@@ -15,14 +16,15 @@ Page({
       {
         url: 'http://p1.music.126.net/Yo-FjrJTQ9clkDkuUCTtUg==/109951164169441928.jpg',
       }
-    ]
+    ],
+    playlist: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this._getPlaylist()
   },
 
   /**
@@ -57,14 +59,17 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.setData({
+      playlist: []
+    })
+    this._getPlaylist()
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this._getPlaylist()
   },
 
   /**
@@ -72,5 +77,25 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  _getPlaylist() {
+    wx.showLoading({
+      title: '加载中',
+    })
+    wx.cloud.callFunction({
+      name: 'music',
+      data: {
+        start: this.data.playlist.length,
+        count: MAX_LIMIT
+      }
+    }).then(res => {
+      console.log(res)
+      this.setData({
+        playlist: this.data.playlist.concat(res.result.data)
+      })
+      wx.stopPullDownRefresh()
+      wx.hideLoading()
+    })
   }
 })
